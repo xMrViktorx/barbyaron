d9972562355edb3c8862b1d6334889635e7916a9
Molnár Viktor <molviktor99@gmail.com>
2021-12-12 12:06:51 +0100

    views

---
+++ resources/views/emails/ContactMail.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>BarbyAron | Kapcsolat</title>
+</head>
+<body>
+  <h3>BarbyAron | Kapcsolat</h3>
+  <p>{{ $data['body'] }}</p>
+  <br>
+  <div style="float:left">
+  <b>Feladó</b><br>
+  <span>{{ $data['name'] }}</span><br>
+  <span>{{ $data['email'] }}</span>
+  </div>
+</body>
+</html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+    <link href="./css/app.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <title>BarbyAron</title>
+    <meta name="description" content= "BarbyAron Portfolio" />
+    <meta name="robots" content= "index, follow">
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
+    </style>
+</head>
+<body>
+    <div id="app" class="flex flex-col min-h-screen" app_url="{{\Config::get('app.url')}}">
+    </div>
+    <script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
